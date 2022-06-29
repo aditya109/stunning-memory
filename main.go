fab059acbba84dea9751fe1b48e95ed69b99a98b
aditya109 <adikid1996@gmail.com>
2022-06-30 01:15:12 +0530

    helm and orchestration complete;
    Signed-off-by: aditya109 <adikid1996@gmail.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,8 +12,16 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/welcome", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("/welcome was hit successfully !")
 		fmt.Fprintf(w, "Welcome to server ! ⚡ ⛈️")
 	})
 
-	http.ListenAndServe(":3000", r)
+	r.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("/status was hit successfully !")
+		fmt.Fprintf(w, "The server is running ! 🏃‍♂️⏩")
+	})
+	log.Print("server running successfully at http://localhost:3000 !")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatalf("server starting failed: %v", err)
+	}
 }

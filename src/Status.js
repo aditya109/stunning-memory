9d909cadc63728484aea5e3ab86f610fae49242d
aditya109 <adikid1996@gmail.com>
2022-07-02 20:37:22 +0530

    reactjs completed;
    Signed-off-by: aditya109 <adikid1996@gmail.com>

---
+++ src/Status.js
@@ -0,0 +1,11 @@
+
+function Status() {
+  console.log('/status was hit successfully !')
+  return (
+    <div>
+      <span>The server is running ! 🏃‍♂️⏩</span>
+    </div>
+  );
+}
+
+export default Status;
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+
+function Welcome() {
+  console.log('/welcome was hit successfully !')
+  return (
+    <div>
+      <span>Welcome to server ! ⚡ ⛈️</span>
+    </div>
+  );
+}
+
+export default Welcome;

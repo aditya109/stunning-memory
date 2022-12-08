fea48dad4b5ffd839dc2c300540768c1790f6c5d
Aditya Kumar <adityakumar@exotel.in>
2022-12-08 11:13:20 +0530

    added changes;
    Signed-off-by: Aditya Kumar <adityakumar@exotel.in>

--- kubernetes/manifests/example/load-testing.sh
+++
@@ -1,4 +0,0 @@
-echo "GET http://192.168.49.2:31504/ping" | vegeta attack -duration=4m -rate=200 | tee results.bin | vegeta report
-vegeta report -type=json results.bin > metrics.json
-cat results.bin | vegeta plot > plot.html
-cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
